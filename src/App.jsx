23a281be0983bc9e4e9d39bce64c41c61277d9ed
Arafath Aleem <arafathaleem111@gmail.com>
2025-04-13 17:33:20 +0530

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">
+        ArafTech Photofolio App
+      </h1>
+    </div>
+  );
+}
+
+export default App;
